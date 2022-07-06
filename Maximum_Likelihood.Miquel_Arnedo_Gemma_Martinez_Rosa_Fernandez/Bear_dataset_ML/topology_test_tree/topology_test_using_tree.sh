010b9e2a0e6ca4f558568458d46bf0c7bee53ab4
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2022-07-06 08:46:06 +0200

    Update topology_test_using_tree.sh

--- Maximum_Likelihood.Miquel_Arnedo_Gemma_Martinez_Rosa_Fernandez/Bear_dataset_ML/topology_test_tree/topology_test_using_tree.sh
+++ Maximum_Likelihood.Miquel_Arnedo_Gemma_Martinez_Rosa_Fernandez/Bear_dataset_ML/topology_test_tree/topology_test_using_tree.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #SBATCH -n 1
 #SBATCH -c 1
-#SBATCH --mem=10G
+#SBATCH --mem=6G
 #SBATCH -o slurm.%j.%x.out
 #SBATCH -e slurm.%j.%x.err
 #SBATCH --time=0-1:0
 
-module load cesga/2020
-module load iq-tree/2.1.2
+#module load
+module load iq-tree
 
-iqtree2 -s 50_genes.fa -z ../bear_species_trees_topologies.tre -te PREVIOUS_TREE
+iqtree -s 50_genes.fa -z ../bear_species_trees_topologies.tre -te PREVIOUS_TREE
