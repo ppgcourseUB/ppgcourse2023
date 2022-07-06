bdedcc6d60ca80c95e49c6f7ca9c0c3c9a1ae6d4
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2022-07-06 08:40:12 +0200

    Update model_finder.sh

--- Maximum_Likelihood.Miquel_Arnedo_Gemma_Martinez_Rosa_Fernandez/Bear_dataset_ML/model_selection/model_finder.sh
+++ Maximum_Likelihood.Miquel_Arnedo_Gemma_Martinez_Rosa_Fernandez/Bear_dataset_ML/model_selection/model_finder.sh
@@ -5,7 +5,7 @@
 #SBATCH -o slurm.%j.%x.out
 #SBATCH -e slurm.%j.%x.err
 
-# module load cesga
+# module load
 module load iq-tree
 
-iqtree2 -s 50_genes.fa -m MF -mset LG+F+G,WAG+F+G,JTT+F+G,GTR20 -madd LG+C20+F+G,LG+C10+F+G,LG+C30+F+G,LG+C40+F+G,LG+C50+F+G,LG+C60+F+G,C10,C20,C30,C40,C50,C60,EX2,EX3,EHO,LG4M,LG4X -T AUTO
+iqtree -s 50_genes.fa -m MF -mset LG+F+G,WAG+F+G,JTT+F+G,GTR20 -madd LG+C20+F+G,LG+C10+F+G,LG+C30+F+G,LG+C40+F+G,LG+C50+F+G,LG+C60+F+G,C10,C20,C30,C40,C50,C60,EX2,EX3,EHO,LG4M,LG4X -nt 8
