54f0d3fe3ce3e045df35172014fb693a7236593b
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2022-07-06 08:43:25 +0200

    Update shalrt_iqtree.sh

--- Maximum_Likelihood.Miquel_Arnedo_Gemma_Martinez_Rosa_Fernandez/Bear_dataset_ML/shalrt/shalrt_iqtree.sh
+++ Maximum_Likelihood.Miquel_Arnedo_Gemma_Martinez_Rosa_Fernandez/Bear_dataset_ML/shalrt/shalrt_iqtree.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #SBATCH -n 1
-#SBATCH -c 10
-#SBATCH --mem=10G
+#SBATCH -c 8
+#SBATCH --mem=6G
 #SBATCH -o slurm.%j.%x.out
 #SBATCH -e slurm.%j.%x.err
 #SBATCH --time=0-5:0
 
-module load cesga/2020
-module load iq-tree/2.1.2
+#module load 
+module load iq-tree
 
-iqtree2 -s 50_genes.fa -m MODEL -alrt 1000 -T AUTO
+iqtree -s 50_genes.fa -m MODEL -alrt 1000 -nt 8
