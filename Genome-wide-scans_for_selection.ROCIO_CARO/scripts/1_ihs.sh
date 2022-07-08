a9b8475654543a283542b12ac0c0ebdd7d529e6b
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2022-07-08 10:43:22 +0200

    Update 1_ihs.sh

--- Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_ihs.sh
+++ Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_ihs.sh
@@ -1,37 +1,36 @@
-#!/bin/bash                                                                                                           
-                                                                                                                        
-# define names                                                                                                                                                                                                                                                                                                                                             
-#SBATCH --job-name=selscan                                                                                              
-#SBATCH --error selscan-%j.err                                                                                          
-#SBATCH --output selsan-%j.out                                                                                          
+#!/bin/bash     
+                                                                                                 
+# define names                                                                                                                                         
+#SBATCH --job-name=selscan
+#SBATCH --error selscan-%j.err
+#SBATCH --output selsan-%j.out 
 
-# memory and CPUs request                                                                                              
-#SBATCH --mem=6G                                                                                                       
-#SBATCH --cpus-per-task=8                                                                                               
+# memory and CPUs request
+#SBATCH --mem=6G
+#SBATCH --cpus-per-task=8
 
 
-#### For CEU ####                                                                                                       
+#### For CEU #### 
 HAP_FILE=data/CEU/CEU.recode.vcf.hap
 MAP_FILE=data/CEU/CEU_filled.map
 IHS_FILE=CEU
 NORM_IHS_FILE=CEU.ihs.out
 
-# iHS computation                                                                                                       
+# iHS computation
 selscan --ihs --hap $HAP_FILE --map $MAP_FILE --out $IHS_FILE --threads 8
 
-# Normalization                                                                                                         
+# Normalization
 norm --ihs --files $NORM_IHS_FILE
 
 
-#### For YRI ####                                                                                                       
-
+#### For YRI ####
 HAP_FILE=data/YRI/YRI.recode.vcf.hap
 MAP_FILE=data/YRI/YRI_filled.map
 IHS_FILE=YRI
 NORM_IHS_FILE=YRI.ihs.out
 
-# iHS computation                                                                                                       
+# iHS computation
 selscan --ihs --hap $HAP_FILE --map $MAP_FILE --out $IHS_FILE --threads 8
 
-# Normalization                                                                                                         
+# Normalization
 norm --ihs --files $NORM_IHS_FILE
