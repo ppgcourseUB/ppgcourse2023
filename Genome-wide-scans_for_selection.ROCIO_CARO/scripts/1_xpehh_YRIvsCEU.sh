eb43196e78d236b0e7ad4a5033d5ee1fa3df2575
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2022-07-08 10:52:33 +0200

    Update 1_xpehh_YRIvsCEU.sh

--- Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_xpehh_YRIvsCEU.sh
+++ Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_xpehh_YRIvsCEU.sh
@@ -8,18 +8,15 @@
 # memory and CPUs request
 #SBATCH --mem=6G
 #SBATCH --cpus-per-task=8
-module load selscan/v1.2.0a
 
+# XPEHH computation for YRI vs CEU
 HAP1=data/YRI/YRI.recode.vcf.hap
 HAP2=data/CEU/CEU.recode.vcf.hap
 MAP=data/CEU/CEU_filled.map
 XPEHH_FILE=YRIvsCEU
-
-NORM_XPEHH_FILE=YRIvsCEU.xpehh.out
-
-# XPEHH computation
 selscan --xpehh --hap $HAP1 --ref $HAP2 --map $MAP --out $XPEHH_FILE --threads 8
 
 # Normalization
+NORM_XPEHH_FILE=YRIvsCEU.xpehh.out
 norm --xpehh --files $NORM_XPEHH_FILE
 
