12d3c1645659d73150a92042ea56a1e61f3224c9
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2022-07-08 10:46:10 +0200

    Update 1_xpehh_YRIvsCEU.sh

--- Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_xpehh_YRIvsCEU.sh
+++ Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_xpehh_YRIvsCEU.sh
@@ -1,32 +1,18 @@
-#!/bin/bash 
+#!/bin/bash     
 
-#SBATCH -J XPEHH_tutorial # A single job name for the array 
+# define names
+#SBATCH --job-name=selscan
+#SBATCH --error selscan-%j.err
+#SBATCH --output selsan-%j.out 
 
-#SBATCH -n 1 # Number of cores (task per node)
-
-#SBATCH -N 1 # All cores on one machine (nodes)
-
+# memory and CPUs request
+#SBATCH --mem=6G
 #SBATCH --cpus-per-task=8
-
-#SBATCH -p normal # Partition 
-
-#SBATCH --mem=6GB # Memory request  
-
-#SBATCH -o XPEHH_tutorial.out # Standard output 
-
-#SBATCH -e XPEHH_tutorial.err # Standard error  
-
-#SBATCH --mail-type=ALL # mail alert at start, end and abortion of execution
-
-#SBATCH --mail-user=rocio.caro@upf.edu # send mail to this address
-
 module load selscan/v1.2.0a
 
-HAP1=YRI.recode.vcf.hap
-HAP2=CEU.recode.vcf.hap
-
-MAP=CEU_filled.map
-
+HAP1=data/YRI/YRI.recode.vcf.hap
+HAP2=data/CEU/CEU.recode.vcf.hap
+MAP=data/CEU/CEU_filled.map
 XPEHH_FILE=YRIvsCEU
 
 NORM_XPEHH_FILE=YRIvsCEU.xpehh.out
