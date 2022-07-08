6afbe794f0a214d3f8f6a77a67f4492e5ffb232f
ppgcourseUB <105917473+ppgcourseUB@users.noreply.github.com>
2022-07-08 10:41:05 +0200

    Update 1_ihs.sh

--- Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_ihs.sh
+++ Genome-wide-scans_for_selection.ROCIO_CARO/scripts/1_ihs.sh
@@ -1,57 +1,37 @@
-#!/bin/bash 
+#!/bin/bash                                                                                                           
+                                                                                                                        
+# define names                                                                                                                                                                                                                                                                                                                                             
+#SBATCH --job-name=selscan                                                                                              
+#SBATCH --error selscan-%j.err                                                                                          
+#SBATCH --output selsan-%j.out                                                                                          
 
-#SBATCH -J iHS_tutorial # A single job name for the array 
-
-#SBATCH -n 1 # Number of cores (task per node)
-
-#SBATCH -N 1 # All cores on one machine (nodes)
-
-#SBATCH --cpus-per-task=8
-
-#SBATCH -p normal # Partition 
-
-#SBATCH --mem=6GB # Memory request  
-
-#SBATCH -o iHS_tutorial.out # Standard output 
-
-#SBATCH -e iHS_tutorial.err # Standard error  
-
-#SBATCH --mail-type=ALL # mail alert at start, end and abortion of execution
-
-#SBATCH --mail-user=YOURMAIL # send mail to this address
-
-module load selscan/v1.2.0a
+# memory and CPUs request                                                                                              
+#SBATCH --mem=6G                                                                                                       
+#SBATCH --cpus-per-task=8                                                                                               
 
 
-#### For CEU ####
-
-HAP_FILE=CEU.recode.vcf.hap
-
-MAP_FILE=CEU_filled.map
-
+#### For CEU ####                                                                                                       
+HAP_FILE=data/CEU/CEU.recode.vcf.hap
+MAP_FILE=data/CEU/CEU_filled.map
 IHS_FILE=CEU
-
 NORM_IHS_FILE=CEU.ihs.out
 
-# iHS computation
+# iHS computation                                                                                                       
 selscan --ihs --hap $HAP_FILE --map $MAP_FILE --out $IHS_FILE --threads 8
 
-# Normalization
+# Normalization                                                                                                         
 norm --ihs --files $NORM_IHS_FILE
 
 
-#### For YRI ####
+#### For YRI ####                                                                                                       
 
-HAP_FILE=YRI.recode.vcf.hap
-
-MAP_FILE=YRI_filled.map
-
+HAP_FILE=data/YRI/YRI.recode.vcf.hap
+MAP_FILE=data/YRI/YRI_filled.map
 IHS_FILE=YRI
-
 NORM_IHS_FILE=YRI.ihs.out
 
-# iHS computation
+# iHS computation                                                                                                       
 selscan --ihs --hap $HAP_FILE --map $MAP_FILE --out $IHS_FILE --threads 8
 
-# Normalization
+# Normalization                                                                                                         
 norm --ihs --files $NORM_IHS_FILE
