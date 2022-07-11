89feaa8b29e34b46e043bcd9a87930e36467c8d2
sguir <52529085+sguir@users.noreply.github.com>
2022-07-11 23:26:58 +0200

    Add files via upload

---
+++ Adaptive_differentiaion_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_stdis_contrast_10000_simulations.sh
@@ -0,0 +1,20 @@
+#!/bin/bash                                                                                                             
+
+# define names                                                                                                          
+#SBATCH --job-name=bp_stdis_contr                                                                                         
+#SBATCH --error bp_stdis_contr-%j.err                                                                                     
+#SBATCH --output bp_stdis_contr-%j.out                                                                                    
+
+# memory and CPUs request                                                                                               
+#SBATCH --mem=6G                                                                                                        
+#SBATCH --cpus-per-task=8 
+
+# directories
+INPUT=../input/hgdp.geno
+cd $INPUT
+
+# module load                                                                                                           
+module load BayPass   
+
+# run BayPass (CORE Model) with the 10000 c2 PODs as input
+./g_baypass -npop 52 -gfile G.hgdp_C2_10000_pods -contrastfile covariates_eu -efile covariates_eu -nthreads 8 -d0yij 20 -outprefix hgdp_contrast_10000_pods 
