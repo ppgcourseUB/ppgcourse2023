3b785068dbb90351b0aef34cf203334d3c3a1334
sguir <52529085+sguir@users.noreply.github.com>
2022-07-13 09:46:15 +0200

    Update run_stdis_contr_model.sh

--- Adaptive_differentiaion_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_stdis_contr_model.sh
+++ Adaptive_differentiaion_and_covariates_association.SARA_GUIRAO-RICO/scripts/run_stdis_contr_model.sh
@@ -17,4 +17,4 @@ cd $INPUT
 module load BayPass   
 
 # run BayPass (STDis and Contrast Model)
-g_baypass -npop 52 -gfile hgdp.geno -contrastfile covariates_eu -efile covariates_eu -nthreads 8 -d0yij 20 -outprefix hgdp_contrast
+g_baypass -npop 52 -gfile hgdp.geno -contrastfile covariates_eu -efile covariates_eu -nthreads 8 -outprefix hgdp_contrast
