9fbabf3029351ce8f4f951ed4d7aaa8dd9da6332
sramosonsins <sebastian.ramos@cragenomica.es>
2022-07-13 16:05:14 +0200

    Update README and Practical

--- PopGenomicsAdaptation.SebastianERamosOnsins/Practical/run_Rresults.sh
+++ PopGenomicsAdaptation.SebastianERamosOnsins/Practical/run_Rresults.sh
@@ -4,10 +4,9 @@
 #SBATCH -o %j.out
 #SBATCH -e %j.err
 #SBATCH --ntasks=2
-#SBATCH --mem=12GB
+#SBATCH --mem=6GB
 #SBATCH --partition=normal
 #
-module load R
 module load r-mass r-proto
 
 srun --ntasks 1 --exclusive --mem-per-cpu=1GB R --vanilla < ./Results_plotMKT.R&
--- PopGenomicsAdaptation.SebastianERamosOnsins/Practical/run_construct_slim_conditions.sh
+++ PopGenomicsAdaptation.SebastianERamosOnsins/Practical/run_construct_slim_conditions.sh
@@ -13,7 +13,7 @@ echo \#SBATCH --job-name=9slims >> ./run_slim_conditions.sh
 echo \#SBATCH -o %j.out >> ./run_slim_conditions.sh
 echo \#SBATCH -e %j.err >> ./run_slim_conditions.sh
 echo \#SBATCH --ntasks=9 >> ./run_slim_conditions.sh
-echo \#SBATCH --mem=12GB >> ./run_slim_conditions.sh
+echo \#SBATCH --mem=6GB >> ./run_slim_conditions.sh
 echo \#SBATCH --partition=normal >> ./run_slim_conditions.sh
 echo \# >> ./run_slim_conditions.sh
 echo module load SLiM >> ./run_slim_conditions.sh
