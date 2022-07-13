6b66040db15c25e113ed2a29272bd04b7e47f823
sramosonsins <sebastian.ramos@cragenomica.es>
2022-07-13 15:59:39 +0200

    Update README and Practical

---
+++ PopGenomicsAdaptation.SebastianERamosOnsins/Practical/run_Rresults.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=2runRplots
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH --ntasks=2
+#SBATCH --mem=12GB
+#SBATCH --partition=normal
+#
+module load R
+module load r-mass r-proto
+
+srun --ntasks 1 --exclusive --mem-per-cpu=1GB R --vanilla < ./Results_plotMKT.R&
+srun --ntasks 1 --exclusive --mem-per-cpu=1GB R --vanilla < ./Results_plotMKT_Theta.R&
+wait
