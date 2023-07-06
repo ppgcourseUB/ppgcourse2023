da4eb6e42bfebe5b8a97a886a015ad4f4e3bf4ab
Lozano-Fernandez <77489969+Lozano-Fernandez@users.noreply.github.com>
2023-07-06 13:57:44 +0200

    Update non_parametric_iqtree.sh

--- Maximum_Likelihood.Miquel_Arnedo_Gemma_Martinez_Rosa_Fernandez/Bear_dataset_ML/non_parametric/non_parametric_iqtree.sh
+++ Maximum_Likelihood.Miquel_Arnedo_Gemma_Martinez_Rosa_Fernandez/Bear_dataset_ML/non_parametric/non_parametric_iqtree.sh
@@ -4,9 +4,9 @@
 #SBATCH --mem=6G
 #SBATCH -o slurm.%j.%x.out
 #SBATCH -e slurm.%j.%x.err
-#SBATCH --time=0-5:0
+#SBATCH --time=0-10:0
 
 #module load 
 module load iq-tree
 
-iqtree -s 50_genes.fa -m LG+C20+F+G -b 50 -nt 8 -redo
+iqtree -s 50_genes.fa -m LG+C20+F+G -b 10 -nt 8 -redo
