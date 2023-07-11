e9c80f3b3653ec30e0eb2526dac43721336e6c75
Sebastian E. Ramos-Onsins <sebastian.ramos@cragenomica.es>
2023-07-11 11:48:09 +0200

    Add files via upload

--- PopGenomicsAdaptation.SebastianERamosOnsins/Practical/run_construct_slim_conditions.sh
+++ PopGenomicsAdaptation.SebastianERamosOnsins/Practical/run_construct_slim_conditions.sh
@@ -20,14 +20,14 @@ echo module load SLiM >> ./run_slim_conditions.sh
 echo >> ./run_slim_conditions.sh
 
 #fixed paraneters
-Ne=500; L=500000; ngenes=100;
+Ne=750; L=500000; ngenes=100;
 mut_rate=1e-6;
 ind_sample_size=25; out_sample_size=1;
 
 # CONDITION 0:
 #Neutral. No change Ne.
 FILEOUT="'./00_slim_SFS_SNM.txt'"
-Neb=500; nsweeps=0;
+Neb=750; nsweeps=0;
 rec_rate=1e-4;
 rate_ben=0; s_backg_ben=0;
 rate_del=0; s_backg_del=0;
@@ -38,7 +38,7 @@ echo srun --ntasks 1 --exclusive --mem-per-cpu=1GB slim -t -m -d \"Ne=$Ne\" -d \
 # CONDITION 1:
 #Strong BACKGROUND SELECTION. No beneficial selection. No change Ne. No sweep
 FILEOUT="'./01_slim_SFS_BGS.txt'"
-Neb=500; nsweeps=0;
+Neb=750; nsweeps=0;
 rec_rate=1e-4
 rate_ben=0; s_backg_ben=0;
 rate_del=9; s_backg_del=-0.005;
@@ -49,7 +49,7 @@ echo srun --ntasks 1 --exclusive --mem-per-cpu=1GB slim -t -m -d \"Ne=$Ne\" -d \
 # CONDITION 2:
 #No background selection. BENEFICIAL SELECTION. No change Ne. No sweep
 FILEOUT="'./02_slim_SFS_PS.txt'"
-Neb=500; nsweeps=0;
+Neb=750; nsweeps=0;
 rec_rate=1e-4
 rate_ben=0.2; s_backg_ben=0.002;
 rate_del=0; s_backg_del=0;
@@ -60,7 +60,7 @@ echo srun --ntasks 1 --exclusive --mem-per-cpu=1GB slim -t -m -d \"Ne=$Ne\" -d \
 # CONDITION 3:
 #Strong BACKGROUND SELECTION. MIDDLE PROPORTION BENEFICIAL SELECTION. No change Ne. No sweep.
 FILEOUT="'./03_slim_SFS_BGS_PS.txt'"
-Neb=500; nsweeps=0;
+Neb=750; nsweeps=0;
 rec_rate=1e-4
 rate_ben=0.2; s_backg_ben=0.005;
 rate_del=8; s_backg_del=-0.002;
